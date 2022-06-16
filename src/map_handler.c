20ce970c94bda0658ec3a74128fe9319c34a97f5
jtomala <jtomala@students.42wolfsburg.de>
2022-06-16 17:06:35 +0200

    map read

--- src/map_handler.c
+++ src/map_handler.c
@@ -1,31 +1,5 @@
 #include "../incl/cub3d.h"
 
-
-// int ft_copy(char *dest, char *src, int len)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	if (len == 0)
-// 	{
-// 		while (src[i])
-// 		{
-// 			dest[i] = src[i];
-// 			i++;
-// 		}
-// 	}
-// 	else
-// 	{
-// 		len--;
-// 		while (i < len && src[i])
-// 		{
-// 			dest[i] = src[i];
-// 			i++;
-// 		}
-// 	}
-// 	return (i);
-// }
-
 /*
 gets triggert when the first character of the line is a alph
 safes all the paths of the directions and the colors of the 
@@ -59,18 +33,13 @@ int	safe_preoptions(t_window *window, char *line)
 gets triggert when the first character of the line is a number
 safes every row of the map in the char** map and counts the size
 */
-int safe_map(t_window *window, char *line, int row)
+int count_map(t_window *window, char *line)
 {
 	int i;
 	int columns;
 
 	i = 0;
 	columns = 0;
-	// window->map->map = ft_calloc(1, sizeof(char **));
-	// window->map->map[row] = ft_calloc(1, sizeof(char *));
-	// ft_copy(window->map->map[window->map->rows], line, 0);
-	//window->map->map[row] = ft_substr(line, 0, ft_strlen(line));
-	printf("%d\n", row);
 	while (line[i])
 	{
 		if (ft_isdigit(line[i]))
@@ -81,6 +50,14 @@ int safe_map(t_window *window, char *line, int row)
 		window->map->columns = columns;
 	return (0);
 }
+
+/*
+safes the map
+*/
+// int safe_map(t_window *window, line)
+// {
+// 	window->map->map = ft_calloc()
+// }
 
 /*
 reads in the map that got passed and calls different functions to 
@@ -106,12 +83,20 @@ int	map_handler(t_window *window)
 			safe_preoptions(window, line);
 		else if (ft_isdigit(line[counter]))
 		{
-			safe_map(window, line, window->map->rows);
+			count_map(window, line); //umbenennen in count_map
 			window->map->rows++;
 		}
 		free(line);
 		line = get_next_line(fd);
 	}
+	//call save map here because you have to know the amount of rows
 	printf("rows: %d\ncolumns: %d\n", window->map->rows, window->map->columns);
 	return (0);
 }
+
+
+/*
+
+**map = ft_calloc(row, sizeof(char *))
+
+*/

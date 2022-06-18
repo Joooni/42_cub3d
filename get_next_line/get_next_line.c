0ca78f9ad307a2bdce9632d44b69c843434b14bf
jtomala <jtomala@students.42wolfsburg.de>
2022-06-18 12:52:12 +0200

    normed get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,17 +6,17 @@
 /*   By: jtomala <jtomala@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 20:07:17 by jtomala           #+#    #+#             */
-/*   Updated: 2022/06/15 20:10:34 by jtomala          ###   ########.fr       */
+/*   Updated: 2022/06/18 12:50:17 by jtomala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char buffer[BUFFER_SIZE + 1];
-	char *output;
-	
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*output;
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	output = (char *) malloc(BUFFER_SIZE + 1);
@@ -26,9 +26,9 @@ char *get_next_line(int fd)
 	return (gnl_handler(fd, buffer, output));
 }
 
-char *gnl_handler(int fd, char *buffer, char *output)
+char	*gnl_handler(int fd, char *buffer, char *output)
 {
-	ssize_t read_result;
+	ssize_t	read_result;
 
 	while (1)
 	{
@@ -50,10 +50,10 @@ char *gnl_handler(int fd, char *buffer, char *output)
 	}
 }
 
-char *ft_found_new_line(char *buffer, char *output)
+char	*ft_found_new_line(char *buffer, char *output)
 {
-	int copy_size;
-	char *tmp;
+	int		copy_size;
+	char	*tmp;
 
 	copy_size = (ft_strchr(buffer, '\n') - buffer) + 1;
 	tmp = (char *) malloc(BUFFER_SIZE + 1);
@@ -65,12 +65,12 @@ char *ft_found_new_line(char *buffer, char *output)
 	return (output);
 }
 
-char *ft_realloc(char *s1, char *s2, int free_s1, int free_s2)
+char	*ft_realloc(char *s1, char *s2, int free_s1, int free_s2)
 {
-	int s1_len;
-	int s2_len;
-	char *output;
-	int i;
+	int		s1_len;
+	int		s2_len;
+	char	*output;
+	int		i;
 
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
@@ -88,7 +88,7 @@ char *ft_realloc(char *s1, char *s2, int free_s1, int free_s2)
 
 int	ft_copy(char *dst, char *src, int len)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (len == 0)
@@ -111,12 +111,3 @@ int	ft_copy(char *dst, char *src, int len)
 	dst[i] = '\0';
 	return (i);
 }
-
-// int main(void)
-// {
-// 	int fd = open("file", O_RDONLY);
-// 	printf("%d\n", fd);
-// 	char *line = get_next_line(fd);
-// 	printf("%s\n", line);
-// 	return (0);
-// }

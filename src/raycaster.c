c6f605972ad7229017b8681ac2b82332c33379a1
Jonas Subel <joooni.su@gmail.com>
2022-06-23 09:56:37 +0200

    almost normed

--- src/raycaster.c
+++ src/raycaster.c
@@ -7,38 +7,34 @@ void	ft_cast_ray(t_window *window, t_rc *rc)
 	ft_calc_hit(*(window->map), window->player);
 }
 
-void	ft_calc_hit(t_map map, t_player *player)
+void	ft_calc_hit(t_map map, t_rc *ray)
 {
-	int	wall_hit;
-	int	wall_side;
+	int		wall_hit;
+	int		wall_side;
 	double	wall_distance;
 
 	wall_hit = 0;
 	while (wall_hit == 0)
 	{
-		if (player->ray->side_dist->x < player->ray->side_dist->y)
+		if (ray->side_dist->x < ray->side_dist->y)
 		{
-			player->ray->side_dist->x += player->ray->delta_dist->x * 32;
-			player->ray->map_pos->x += player->ray->step.x;
-			printf("ray cast in x direction!\n");
+			ray->side_dist->x += ray->delta_dist->x * 32;
+			ray->map_pos->x += ray->step.x;
 			wall_side = 0;
 		}
 		else
 		{
-			player->ray->side_dist->y += player->ray->delta_dist->y * 32;
-			player->ray->map_pos->y += player->ray->step.y;
-			printf("ray cast in y direction!\n");
+			ray->side_dist->y += ray->delta_dist->y * 32;
+			ray->map_pos->y += ray->step.y;
 			wall_side = 1;
 		}
-		if (map.map[(int)player->ray->map_pos->y][(int) player->ray->map_pos->x] == '1')
+		if (map.map[(int)ray->map_pos->y][(int) ray->map_pos->x] == '1')
 			wall_hit = 1;
-		printf("new side dist:\nx: %f	y: %f\n", player->ray->side_dist->x, player->ray->side_dist->y);
 	}
 	if (wall_side == 0)
-		wall_distance = (player->ray->side_dist->x - player->ray->delta_dist->x) * 32;
+		wall_distance = (ray->side_dist->x - ray->delta_dist->x) * 32;
 	else
-		wall_distance = (player->ray->side_dist->y - player->ray->delta_dist->y) * 32;
-	printf("wall hit! distance to wall from camera plane: %f\nCell coordinates: (%d/%d)\n", wall_distance, player->ray->map_pos->x, player->ray->map_pos->y);
+		wall_distance = (ray->side_dist->y - ray->delta_dist->y) * 32;
 }
 
 void	ft_calc_step(t_window *window, t_rc *rc)
@@ -46,24 +42,27 @@ void	ft_calc_step(t_window *window, t_rc *rc)
 	if (rc->dir->x < 0)
 	{
 		rc->step.x = -1;
-		rc->side_dist->x = (window->player->pos->x - rc->map_pos->x * 32) * rc->delta_dist->x;
+		rc->side_dist->x = (window->player->pos->x
+				- rc->map_pos->x * 32) * rc->delta_dist->x;
 	}
 	else
 	{
 		rc->step.x = 1;
-		rc->side_dist->x = (32 * (rc->map_pos->x + 1.0) - window->player->pos->x) * rc->delta_dist->x;
+		rc->side_dist->x = (32 * (rc->map_pos->x + 1.0)
+				- window->player->pos->x) * rc->delta_dist->x;
 	}
 	if (rc->dir->y < 0)
 	{
 		rc->step.y = -1;
-		rc->side_dist->y = (window->player->pos->y - rc->map_pos->y * 32) * rc->delta_dist->y;
+		rc->side_dist->y = (window->player->pos->y
+				- rc->map_pos->y * 32) * rc->delta_dist->y;
 	}
 	else
 	{
 		rc->step.y = 1;
-		rc->side_dist->y = (32 * (rc->map_pos->y + 1.0) - window->player->pos->y) * rc->delta_dist->y;
+		rc->side_dist->y = (32 * (rc->map_pos->y + 1.0)
+				- window->player->pos->y) * rc->delta_dist->y;
 	}
-	printf("	side distances:\nx: %f	y: %f\n", rc->side_dist->x, rc->side_dist->y);
 }
 
 void	ft_init_rc(t_window *window, t_rc *rc)
@@ -81,5 +80,4 @@ void	ft_init_rc(t_window *window, t_rc *rc)
 		rc->delta_dist->x = INFINITY;
 	else
 		rc->delta_dist->y = fabs(1 / rc->dir->y);
-	printf("	delta dist:\nx: %f	y: %f\n", rc->delta_dist->x, rc->delta_dist->y);
 }

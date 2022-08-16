70f87e885a1eea3388a5a273976cac1923a5d314
Jonas Subel <joooni.su@gmail.com>
2022-08-16 10:03:32 +0200

    fixed minimap

--- src/draw_map.c
+++ src/draw_map.c
@@ -2,21 +2,19 @@
 
 static void	ft_scale_factor(t_window *window, int map_flag)
 {
-	int multiplier;
+	int	multiplier;
 
 	multiplier = 1;
 	if (map_flag == 1)
 		multiplier = 3;
-	window->scale_factor = (double)
-		(WINDOW_WIDTH
-			* M_MINIMAP_FACTOR
-			/ window->map->columns) * multiplier;
-	if (WINDOW_HEIGHT * M_MINIMAP_FACTOR / window->map->rows
-		< window->scale_factor)
+	if (
+		(WINDOW_WIDTH * MINIMAP_FACTOR / window->map->columns)
+		> (WINDOW_HEIGHT * MINIMAP_FACTOR / window->map->rows))
 		window->scale_factor = (double)
-			(WINDOW_HEIGHT
-				* M_MINIMAP_FACTOR
-				/ window->map->rows);
+			(WINDOW_HEIGHT * MINIMAP_FACTOR / window->map->rows) * multiplier;
+	else
+		window->scale_factor = (double)
+			(WINDOW_WIDTH * MINIMAP_FACTOR / window->map->columns) * multiplier;
 }
 
 static void	draw_something(t_window *window, int x, int y, int color)
@@ -31,30 +29,32 @@ static void	draw_something(t_window *window, int x, int y, int color)
 	ft_draw_rect(window, rect);
 }
 
-static int	player_flag(t_window *window, int y, int x)
-{
-	char	**map;
+// static int	player_flag(t_window *window, int y, int x)
+// {
+// 	char	**map;
 
-	map = window->map->map;
-	if (map[y][x] != 'N' && map[y][x] != 'N' \
-		&& map[y][x] != 'E' && map[y][x] != 'E' \
-		&& map[y][x] != 'S' && map[y][x] != 'S' \
-		&& map[y][x] != 'W' && map[y][x] != 'W')
-		return (0);
-	return (1);
-}
+// 	map = window->map->map;
+// 	if (map[y][x] != 'N' && map[y][x] != 'N' \
+// 		&& map[y][x] != 'E' && map[y][x] != 'E' \
+// 		&& map[y][x] != 'S' && map[y][x] != 'S' \
+// 		&& map[y][x] != 'W' && map[y][x] != 'W')
+// 		return (0);
+// 	return (1);
+// }
 
 static int	ft_draw_map_helper(t_window *window, int x, int y)
 {
-	if (window->map->map[y][x] == '1')
+	if (window->map->map[y][x] == MAP_WALL)
 		draw_something(window, x, y, 0x00303096);
 	else if ((y == (int)window->player->pos->y / 32 && x == (int) \
-		window->player->pos->x / 32) || player_flag(window, y, x))
-		draw_something(window, x, y, 0x006C6C6C);
-	else if (window->map->map[y][x] == '0')
+		window->player->pos->x / 32))
+		draw_something(window, x, y, 0x00C7565F);
+	else if (window->map->map[y][x] == MAP_EMPTY_SPACE
+		|| window->map->map[y][x] == MAP_DOOR_OPEN
+		|| ft_strchr(PLAYER_POS, window->map->map[y][x]))
 		draw_something(window, x, y, 0x00353542);
-	else if (window->map->map[y][x] == '2')
-		draw_something(window, x, y, 0x00686a6e);
+	else if (window->map->map[y][x] == MAP_DOOR_CLOSED)
+		draw_something(window, x, y, 0x00686A6E);
 	if (window->map->map[y][x] == '\n')
 		return (1);
 	return (0);
